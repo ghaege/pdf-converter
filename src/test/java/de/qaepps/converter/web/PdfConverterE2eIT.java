43d60ec4a52b2c6d23cc831a8970bacf24d281ae
Gerhard Haege <ghaege@qaeppse.de>
2020-12-19 10:29:51 +0000

    to testcontainers

--- src/test/java/de/qaepps/converter/web/PdfConverterE2eIT.java
+++ src/test/java/de/qaepps/converter/web/PdfConverterE2eIT.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.File;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.DisplayName;
@@ -22,56 +23,80 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+/**
+ * end-to-end tests with dockerized pdf-converter
+ * <p>
+ * Testcontainers creates and runs a image-on-the-fly with pdf-converter included<br>
+ * After the tests the image will be removed
+ * 
+ * @precondition packed pdf-converter-$version.jar (gradle build) and running docker-daemon
+ * 
+ * @author haege
+ *
+ */
 @Tag("e2eTest")
+@Testcontainers
 class PdfConverterE2eIT {
 
-  private static final Logger LOG = LoggerFactory.getLogger(PdfConverterE2eIT.class);
-  
-  private static final String REQUEST_URL = "http://localhost:8100/convert/toPdf";
+	private static final Logger LOG = LoggerFactory.getLogger(PdfConverterE2eIT.class);
 
-//  @BeforeEach
-//  public void init() {
-//      dokumentITUtil.fixHeadless();
-//      requestUrl = configService.getOpenOfficeConverterUrl() + "/convert/toPdf";
-//  }
-  
-  @ParameterizedTest
-  @ValueSource(strings = { "any.docx", "any.dotx", "any.xlsx", "any.xltx"})
-  @DisplayName("should convert office docs to pdf")
-  void convertOfficeDocsToPdf(String filename) {
-	  // prepare	  
-	  HttpEntity<MultiValueMap<String, Object>> requestEntity = createRequestEntity(filename);
-	  
-	  // test
-	  ResponseEntity<byte[]> response = new RestTemplate().postForEntity(REQUEST_URL, requestEntity, byte[].class);
-	  
-	  // validate
-	  assertNotNull(response);
-	  assertThat(response.getStatusCode(), is(HttpStatus.OK));
-  }
+	private static final String API = "/convert/toPdf";
+	private static final int EXPOSED_PORT = 8100;
 
-  @Test
-  @DisplayName("should convert docx to pdf 500 times")
-  void stressConverter()  {
-	  // prepare
-	  HttpEntity<MultiValueMap<String, Object>> requestEntity = createRequestEntity("any.docx");
-	  
-	  // test
-	  int limit = 500;
-	  RestTemplate restTemplate = new RestTemplate();
-	  ResponseEntity<byte[]> response;
-	  LOG.info("stress pdf-converter with {} conversions", limit);
-	  for (int i = 0; i < limit; i++) {
-		  long started = System.currentTimeMillis();
-		  response = restTemplate.postForEntity(REQUEST_URL, requestEntity, byte[].class);
-		  LOG.info("conversion no {} took {} millis", i, System.currentTimeMillis() - started);
-		  
-		  assertNotNull(response);
-		  assertThat(response.getStatusCode(), is(HttpStatus.OK));
-	  } 
-  }
-  
+	private static String getUrl() {
+		return "http://" + appContainer.getHost() + ":" + appContainer.getMappedPort(EXPOSED_PORT) + API;
+	}
+
+	@Container
+	public static GenericContainer<?> appContainer = new GenericContainer<>(
+			new ImageFromDockerfile()
+			.withDockerfile((new File("./Dockerfile")).toPath())
+			).waitingFor(
+					Wait.forLogMessage(".*Started PdfConverterApplication.*", 1)
+					);
+
+	@ParameterizedTest
+	@ValueSource(strings = { "any.docx", "any.dotx", "any.xlsx", "any.xltx"})
+	@DisplayName("should convert office docs to pdf")
+	void convertOfficeDocsToPdf(String filename) {
+		// prepare	  
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = createRequestEntity(filename);
+
+		// test
+		ResponseEntity<byte[]> response = new RestTemplate().postForEntity(getUrl(), requestEntity, byte[].class);
+
+		// validate
+		assertNotNull(response);
+		assertThat(response.getStatusCode(), is(HttpStatus.OK));
+	}
+
+	@Test
+	@DisplayName("should convert docx to pdf 500 times")
+	void stressConverter()  {
+		// prepare
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = createRequestEntity("any.docx");
+
+		// test
+		int limit = 500;
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<byte[]> response;
+		LOG.info("stress pdf-converter with {} conversions", limit);
+		for (int i = 0; i < limit; i++) {
+			long started = System.currentTimeMillis();
+			response = restTemplate.postForEntity(getUrl(), requestEntity, byte[].class);
+			LOG.info("conversion no {} took {} millis", i, System.currentTimeMillis() - started);
+
+			assertNotNull(response);
+			assertThat(response.getStatusCode(), is(HttpStatus.OK));
+		} 
+	}
+
 	private HttpEntity<MultiValueMap<String, Object>> createRequestEntity(String fileName) {
 		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
 		body.add("file", new ClassPathResource("/officefiles/" + fileName));
@@ -86,5 +111,5 @@ class PdfConverterE2eIT {
 		return headers;
 	}
 
-  
+
 }
